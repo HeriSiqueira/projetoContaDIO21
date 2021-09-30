cff3ca4494da704821e6032e332c78d773d38421
Herivelton Siqueira <herivelton-si@hotmail.com>
2021-09-30 15:59:56 -0300

    PRojeto Conta

---
+++ ProjetoDio21/Entities/Conta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjetoDio21.Entities.Enums;
+
+
+
+namespace ProjetoDio21.Entities
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("Saldo Insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDepo)
+        {
+            this.Saldo += valorDepo;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Tranferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Tipo da Conta " + this.TipoConta + " | ";
+            retorno += "Nome: " + this.Nome + " | ";
+            retorno += "Saldo: R$ " + this.Saldo + " | ";
+            retorno += "Crédito: R$ " + this.Credito;
+            return retorno;
+        }
+
+    }
+}
---
+++ ProjetoDio21/Entities/Enums/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ProjetoDio21.Entities.Enums
+{
+    enum TipoConta
+    {
+        PessoaFisica = 1,
+        PEssoaJuridica = 2
+    }
+}
